3914fe1632f7b7663f1500a24c720b62bf6590d9
suqi <zityel86705@chacuo.net>
2022-10-13 18:17:24 +0800

    feat: get wallheven pic

--- src/spider/spider.service.ts
+++ src/spider/spider.service.ts
@@ -16,63 +16,83 @@ export class SpiderService {
     return 'This action adds a new spider';
   }
 
-  // async findAll() {
-  //   const urls: string[] = [];
-  //   const baseUrl = 'https://www.jpmn5.com';
-  //   const getCosPlay = async () => {
-  //     const data = await this.httpService.axiosRef
-  //       .get('https://www.jpmn5.com/Cosplay/Cosplay10772.html')
-  //       .then((res) => {
-  //         const $ = cheerio.load(res.data);
-  //         $('.article-content p img').each(function () {
-  //           // console.log($(this).attr('src'));
-  //           urls.push(baseUrl + $(this).attr('src'));
-  //         });
-  //         // console.log(urls);
-  //         // return res.data;
-  //       });
-  //     // console.log(data);
-  //   };
-  //   await getCosPlay();
-  //   console.log('urls', urls);
-  //   this.writeFile(urls);
-  //   return 'This action returns all spider';
-  // }
   async findAll() {
-    const baseUrl = 'https://www.jpmn5.com';
-    const next = '下一页';
-    let index = 0;
     const urls: string[] = [];
+    const index = 0;
+    const getSize = 3;
+    const webSite = 'https://wallhaven.cc/toplist';
     const getCosPlay = async () => {
-      console.log(index);
-      await this.httpService.axiosRef
-        .get(
-          `https://www.jpmn5.com/Cosplay/Cosplay10772${
-            index ? '_' + index : ''
-          }.html`,
-        )
-        .then(async (res) => {
-          //console.log(res.data)
-          const $ = cheerio.load(res.data);
-          const page = $('.article-content .pagination a')
-            .map(function () {
-              return $(this).text();
-            })
-            .toArray();
-          if (page.includes(next)) {
-            $('.article-content p img').each(function () {
-              console.log($(this).attr('src'));
-              urls.push(baseUrl + $(this).attr('src'));
+      // 获取getSize页的图片
+      for (let i = 0; i < getSize; i++) {
+        console.log('获取第' + (i + 1) + '页');
+        await this.httpService.axiosRef
+          .get(`${webSite}${index === 0 ? '' : '?page=' + index}`)
+          .then((res) => {
+            const $ = cheerio.load(res.data);
+            $('.thumb-listing-page ul li').each(function () {
+              let url = $(this).find('figure img').attr('data-src');
+              url = url.replace('th.wallhaven.cc', 'w.wallhaven.cc');
+              url = url.replace('small', 'full');
+              url = url.replace(
+                url.split('/')[url.split('/').length - 1],
+                'wallhaven-' + url.split('/')[url.split('/').length - 1],
+              );
+              urls.push(url);
             });
-            index++;
-            await getCosPlay();
-          }
+          });
+        // 暂停1秒
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(null);
+          }, 1000);
         });
+      }
     };
     await getCosPlay();
-    console.log(urls);
-    this.writeFile(urls);
+    console.log('urls', urls);
+    // this.writeFile(urls);
+
+    return {
+      code: 200,
+      webSite,
+      data: urls,
+    };
   }
+  // async findAll() {
+  //   const baseUrl = 'https://www.jpmn5.com';
+  //   const next = '下一页';
+  //   let index = 0;
+  //   const urls: string[] = [];
+  //   const getCosPlay = async () => {
+  //     console.log(index);
+  //     await this.httpService.axiosRef
+  //       .get(
+  //         `https://www.jpmn5.com/Cosplay/Cosplay10772${
+  //           index ? '_' + index : ''
+  //         }.html`,
+  //       )
+  //       .then(async (res) => {
+  //         //console.log(res.data)
+  //         const $ = cheerio.load(res.data);
+  //         const page = $('.article-content .pagination a')
+  //           .map(function () {
+  //             return $(this).text();
+  //           })
+  //           .toArray();
+  //         if (page.includes(next)) {
+  //           $('.article-content p img').each(function () {
+  //             console.log($(this).attr('src'));
+  //             urls.push(baseUrl + $(this).attr('src'));
+  //           });
+  //           index++;
+  //           await getCosPlay();
+  //         }
+  //       });
+  //   };
+  //   await getCosPlay();
+  //   console.log(urls);
+  //   this.writeFile(urls);
+  // }
   writeFile(urls: string[]) {
     urls.forEach(async (url) => {
       const buffer = await this.httpService
